c7618da94f30d066633e3ae597b0e81eb8738046
phspopos <phspopos@naver.com>
2025-03-28 16:26:30 +0900

    깃펄스트

--- src/test/GitFirst.java
+++ src/test/GitFirst.java
@@ -3,8 +3,8 @@ package test;
 public class GitFirst {
 
 	public static void main(String[] args) {
-		System.out.println("1.github연동");
-		System.out.println("2번째 잘되나");
-		System.out.println("3번째 잘되나");
+		System.out.println("4.github연동");
+		System.out.println("5번째 잘되나");
+		System.out.println("6번째 잘되나");
 	}
 }
